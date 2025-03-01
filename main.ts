7f100e12ed193d2f61872db9d032b46de06dbb78
bryan6970 <105858406+bryan6970@users.noreply.github.com>
2025-03-01 17:20:46 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 // anything outside a function or class runs on startup
-let maincounter: number = 0
 
+
+serial.redirectToUSB()
+
+let maincounter: number = 0
 basic.forever(function () {
-    maincounter++;
-    serial.writeLine(`Loop number: ${maincounter} `);
-    basic.pause(1000);
+    maincounter += 1
+    serial.writeLine(`Loop number: ${maincounter} `)
+    basic.pause(1000)
 })
